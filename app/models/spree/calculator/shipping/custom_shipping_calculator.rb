55d9a4191f29b28c1968079685bd53ed456d22f8
Lucas Eduardo <ksukoshidesu@live.com>
2019-05-14 16:20:02 -0300

    Update custom_shipping_calculator.rb

--- app/models/spree/calculator/shipping/custom_shipping_calculator.rb
+++ app/models/spree/calculator/shipping/custom_shipping_calculator.rb
@@ -14,7 +14,7 @@ module Spree
       end
 
       def calcula
-        @line_item = Spree::LineItem.find(1).weight => :weight
+        weight = Spree::LineItem.find(1).weight
         frete = Correios::Frete::Calculador.new :cep_origem => "22790-671",
                                                 :cep_destino => :zipcode,
                                                 :peso => :weight,
