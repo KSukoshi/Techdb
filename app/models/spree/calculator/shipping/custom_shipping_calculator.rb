2fd6330eda6858132c2c8ccef4c3c2c24ab5ee1c
Lucas Eduardo <ksukoshidesu@live.com>
2019-05-16 14:15:49 -0300

    Update custom_shipping_calculator.rb

--- app/models/spree/calculator/shipping/custom_shipping_calculator.rb
+++ app/models/spree/calculator/shipping/custom_shipping_calculator.rb
@@ -14,13 +14,13 @@ module Spree
       end
 
       def calcula
-        binding.pry
+        variant_correios = Spree::LineItem.last.variant_id
         frete = Correios::Frete::Calculador.new :cep_origem => "01207-000",
-                                                :cep_destino => :zipcode,
-                                                :peso => Spree::Variant.select(:weight),
-                                                :comprimento => Spree::Variant.select(:depth),
-                                                :largura => Spree::Variant.select(:width),
-                                                :altura => Spree::Variant.select(:height)
+                                                :cep_destino => "03615-080",
+                                                :peso => Spree::Variant.find(variant_correios).weight,
+                                                :comprimento => Spree::Variant.find(variant_correios).depth,
+                                                :largura => Spree::Variant.find(variant_correios).width,
+                                                :altura => Spree::Variant.find(variant_correios).height
 
         servicos = frete.calcular :sedex, :pac
         servicos[:sedex].valor
