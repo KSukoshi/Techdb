8861580308b8c8f5aad65d9fbcf887706badb3e3
Lucas Eduardo <ksukoshidesu@live.com>
2019-06-03 14:59:59 -0300

    Update stocklog_controller.rb

--- app/controllers/spree/admin/stocklog_controller.rb
+++ app/controllers/spree/admin/stocklog_controller.rb
@@ -1,10 +1,7 @@
 module Spree
   module Admin
     class Spree::Admin::StocklogController < Spree::Admin::BaseController
-      before_action :find_movements
       def index
-      end
-      def find_movements
         @stockmovelog = Spree::StockMovement.order(:id)
       end
     end
