75919aa4cd4da3655b2ade58ac3ead3254a94af5
Lucas Eduardo <ksukoshidesu@live.com>
2019-06-03 15:25:02 -0300

    Update stocklog_controller.rb

--- app/controllers/spree/admin/stocklog_controller.rb
+++ app/controllers/spree/admin/stocklog_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class Spree::Admin::StocklogController < Spree::Admin::BaseController
       def index
-        @stockmovelog = Spree::StockMovement.all
+        @stockmovelog = Spree::StockMovement.select("DISTINCT id, quantity, created_at, updated_at, action")
       end
     end
   end
