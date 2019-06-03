956395a51653a63e6a6877a7a735fb88a526a9f1
Lucas Eduardo <ksukoshidesu@live.com>
2019-06-03 11:51:40 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-Spree::Core::Engine.routes.prepend do
-  namespace :admin do
-    match '/stocklog', :to => 'stocklog#index', :as => :stocklog
+  Spree::Core::Engine.routes.prepend do
+    namespace :admin do
+      match '/stocklog', :to => 'stocklog#index', :as => :stocklog
+    end
   end
 end
