807056b871f078465b017c290efad1f21b3bb2e2
Lucas Eduardo <ksukoshidesu@live.com>
2019-07-16 13:36:03 -0300

    Create frontend.js

---
+++ app/assets/javascripts/spree/frontend/frontend.js
@@ -0,0 +1,43 @@
+//= require spree
+//= require spree/frontend/checkout
+//= require spree/frontend/product
+//= require spree/frontend/cart
+//= require spree/frontend/locale_selector
+
+
+$(document).ready(function() {
+
+  $(window).scroll(function () {
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 200) {
+      $('#header').addClass('header-fixed animated slideInDown');
+      $('#header-bar').fadeIn().addClass('animated slideInDown');
+    }
+    if ($(window).scrollTop() < 5)  {
+      $(".header-fixed").removeClass('header-fixed animated slideInDown');
+      $('#header-bar').fadeOut().remmoveClass('animated slideInDown');
+    }
+  });
+});
+
+function run(interval, frames) {
+    var int = 1;
+
+    function func() {
+        document.getElementById("landpage").className = "b"+int;
+        int++;
+        if(int === frames) { int = 1; }
+    }
+
+    var swap = window.setInterval(func, interval);
+}
+
+run(3000, 4); //milliseconds, frames
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
